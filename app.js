48cbe488bccac44c30eaa0aa0660c76232ca0d7f
Bhavishya chauhan <bhavishya2468@gmail.com>
2025-08-22 14:34:06 +0530

    changes added

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ const app = express();
 
 
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use( bodyParser.json({limit: '50mb'}) );
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+  parameterLimit:50000
+}));
 
 
 
