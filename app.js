c9e01ac90533ee417b1e8de99bff09e8262ea006
Bhavishya chauhan <bhavishya2468@gmail.com>
2025-08-22 14:23:17 +0530

    changes added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require('path');
 dotenv.config();
 const app = express();
 
-app.use(bodyParser.json());
+
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
